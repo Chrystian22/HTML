26bd4718303e83d166570705c171ddc305873813
Chrystian22 <crystofer2209@gmail.com>
2022-10-25 18:44:23 -0500

    U-Update 25//10/2022

---
+++ SEMANA 12 Chrystian Roa/Quokka Arrays/Javascript.js
@@ -0,0 +1,9 @@
+//Programa que permita crear un array – vector con los nombres de la semana, luego permita consultar y mostrar por pantalla cualquier día//
+var dias_semana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+var primerdia=dias_semana[3];
+console.log(primerdia);
+
+var x=1
+var y=9
+var suma=(x+y)
+console.log(suma)
